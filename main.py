179223ee99b48ba027f8a3c6253ca16be6c483aa
Tazio <a.giusti@selecta.ve.it>
2020-10-29 14:09:43 +0100

    new numbers

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 def send_message(num: str):
     url = "https://rest.clicksend.com/v3/sms/send"
-    payload = "{\n  \"messages\": [\n    {\n      \"body\":\"Gentile socio\\n L'ASD Il Delfino ti informa che prestissimo partiranno dei corsi e dei programmi di allenamento on line.\\n Se sei interessato non esitare a contattarci.\\n Chiara 3409342799\\n Yero 3464954075\",\n      \"to\": \"" + num + "\",\n      \"from\": \"IlDelfino\"\n    }\n  ]\n} "
+    payload = "{\n  \"messages\": [\n    {\n      \"body\":\"ASD IL DELFINO E' ONLINE\\n Aderendo a questa iniziativa il tuo abbonamento resta attivo.\\n Per conferma scrivi SI ai seguenti numeri\\n Chiara 3409342799\\n Yero 3464954075\\n\",\n      \"to\": \""+num+"\",\n      \"from\": \"IlDelfino\"\n    }\n  ]\n} "
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Basic YW5kcmVhdGdpdXN0aUBnbWFpbC5jb206NUFBQThDRUYtNjA5RS02MDE5LTkyODUtRTBGQzE5RTc4RURG'
@@ -16,7 +16,7 @@ def send_message(num: str):
 
 already_sent = []
 failed = open('errors.txt', 'w')
-with open('numbers1.txt', 'r') as f:
+with open('numbers2.txt', 'r') as f:
     for line in f:
         line = line.strip()
         if len(line) != 10:
@@ -30,6 +30,7 @@ with open('numbers1.txt', 'r') as f:
         if res != 200:
             print(f'Send to Number {line} failed.')
             failed.write(f'{line}\r\n')
+            already_sent.append(line)
             continue
         already_sent.append(line)
         print(f'DONE.\n\n')
